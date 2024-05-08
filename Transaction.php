2b7243de87c5c784b8d9e648bf5fa4ac4f3a4584
Sarjak Bhandari <bhandarisarjak@gmail.com>
2024-05-07 18:56:45 -0700

    basic layount of transaction page

--- Transaction.php
+++ Transaction.php
@@ -1,4 +1,27 @@
 <?php
     require_once("conn.php");
     
+    $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>E-Mart</title>
+    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="icon" type="image/png" href="makeup.png" sizes="32x32">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="banner">
+        <span class="topban">
+            <div class="navbar">
+                <img src="makeup.png" class="logo">
+                <h1 class="logotxt">E-Mart</h1>
+                <ul>
+                    <li><a href="products.php">My Orders</a></li>
+                </ul>
+            </div>
+</span>
+</body>
+</html>
